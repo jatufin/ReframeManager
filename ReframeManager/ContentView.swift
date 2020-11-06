9d1f92734761cebebb91078332dd08b6b015af5e
Janne Tuukkanen <janne@ibfekna.com>
2020-11-06 23:16:57 +0200

    Initial Commit

---
+++ ReframeManager/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ReframeManager
+//
+//  Created by Janne Tuukkanen on 6.11.2020.
+//  Copyright © 2020 Ibfekna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
