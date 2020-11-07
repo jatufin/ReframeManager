3d33181c81d53568e2f786dffb076a33a1d051c9
Janne Tuukkanen <janne@ibfekna.com>
2020-11-08 00:36:02 +0200

    Methods to read video directory

--- ReframeManager/Model/ReframeManagerModel.swift
+++ ReframeManager/Model/ReframeManagerModel.swift
@@ -14,11 +14,12 @@ struct FileExtensions {
     static let reframe = "reframe"
 }
 
-class FileItem {
+struct FileItem {
     var name: String
     var size: Int
-    var creationTime: Date
-    var modificationTime: Date
+    var creationDate: Date
+    var modificationDate: Date
+    var type: String
     
     var videoName: String {
         let a = name.components(separatedBy: ".")
@@ -29,32 +30,29 @@ class FileItem {
         let a = name.components(separatedBy: ".")
         return a[a.count - 1]
     }
-
-    internal init(name: String, size: Int, creationTime: Date, modificationTime: Date) {
-        self.name = name
-        self.size = size
-        self.creationTime = creationTime
-        self.modificationTime = modificationTime
-    }
 }
 
-class Video360File: FileItem {
+struct Video360File {
+    var fileItem: FileItem
+    
     var creationTimeStamp: String {
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd-HH-mm-ss-000ZZZ"
-        return df.string(from: creationTime)
+        return df.string(from: fileItem.creationTime)
     }
     
     // This reframe file name is used by the Player app
     var reframeEditFileName: String {
-        "\(creationTimeStamp)-\(size).\(FileExtensions.reframe)"
+        "\(creationTimeStamp)-\(fileItem.size).\(FileExtensions.reframe)"
     }
 }
 
-class ReframeFile: FileItem {
+struct ReframeFile  {
+    var fileItem: FileItem
+    
     // Part of the file name between the first and last dots
     var reframeName: String {
-        let a = name.components(separatedBy: ".")
+        let a = fileItem.name.components(separatedBy: ".")
         var s = ""
         for i in 1..<a.count {
             if i > 1 { s += "." }
@@ -72,10 +70,72 @@ struct Video360 {
     var highDef360File: Video360File?
     var lowDef360File: Video360File?
     
-    var reframeFiles: [ReframeFile]
+    var reframeFiles = [ReframeFile]()
+    var otherFiles = [FileItem]()
+    
+    mutating func addFile(fileItem: FileItem) {
+        switch fileItem.fileExtension {
+            case FileExtensions.highDef:
+                highDef360File = Video360File(fileItem: fileItem)
+            case FileExtensions.lowDef:
+                lowDef360File = Video360File(fileItem: fileItem)
+            case FileExtensions.reframe:
+                reframeFiles.append(ReframeFile(fileItem: fileItem))
+        default:
+            otherFiles.append(fileItem)
+        }
+        
+    }
 }
 
 struct Directory {
     var url: URL
-    var videos: [String : Video360]
+    var readable: Bool = true
+    var videos = [String : Video360]()
+    
+    init(path: String) {
+        self.url = URL(fileURLWithPath: path)
+    }
+    
+    private mutating func loadDir() -> [FileItem] {
+        var fileItems = [FileItem]()
+        let fileManager = FileManager.default
+        
+        do {
+            let files = try fileManager.contentsOfDirectory(atPath: url.path)
+            
+            for file in files {
+                let fileItem = try getFileInfo(file: file, fileManager: fileManager)
+                
+                fileItems.append(fileItem)
+            }
+        } catch {
+            readable = false
+        }
+        
+        return fileItems
+    }
+    
+    private func getFileInfo(file: String, fileManager: FileManager) throws -> FileItem {
+        var fileURL = self.url
+        fileURL.appendPathComponent(file)
+        
+        let attributes = try fileManager.attributesOfItem(atPath: fileURL.path)
+        
+        let modificationDate = attributes[FileAttributeKey.modificationDate] as? Date ?? Date(timeIntervalSince1970: 0)
+        let creationDate = attributes[FileAttributeKey.creationDate] as? Date ?? Date(timeIntervalSince1970: 0)
+        let size = attributes[FileAttributeKey.size] as? Int ?? 0
+        let type = attributes[FileAttributeKey.type] as? String ?? "unknown"
+    
+        return FileItem(
+            name: file,
+            size: size,
+            creationDate: creationDate,
+            modificationDate: modificationDate,
+            type: type
+        )
+    }
+    
+    
+    
 }
